f2d613444b3b46574e29dfff2d498e5366cc6c2f
Tomasz Rytwinski <tomasz.rytwinski@capgemini.com>
2017-02-15 14:20:56 +0100

    Poprawiona wersja, wciaz nie obiektowo

--- src/com/capgemini/bowling/BowlingGame.java
+++ src/com/capgemini/bowling/BowlingGame.java
@@ -1,82 +1,82 @@
 package com.capgemini.bowling;
 //Gra w kregle, funkcja int score zwraca obecny wynik gry, funkcja
 public class BowlingGame implements BowlingGameResultCalculator {
-	private int[][] TablicaWynikow = new int[4][12]; 
+	private int[][] tableOfScore = new int[4][12]; 
 
-	private int kolejka=0;
-	private int rzut=0;
+	private int round=0;
+	private int roll=0;
 	
 	
 	public BowlingGame(){
 		for (int i=0;i<10;i++){
 			for(int j=0;j<=3;j++){
-				TablicaWynikow[j][i]=0;
+				tableOfScore[j][i]=0;
 			}
 		}
 	}
 
-	private void DodajBonus(int _kolejka, int numberOfPins){
-		TablicaWynikow[2][_kolejka]=TablicaWynikow[2][_kolejka]+numberOfPins;
+	private void addBonus(int _kolejka, int numberOfPins){
+		tableOfScore[2][_kolejka]=tableOfScore[2][_kolejka]+numberOfPins;
 		
 	}
 	public void roll(int numberOfPins) throws IllegalStateException{
-		if (numberOfPins>10 || (TablicaWynikow[0][kolejka]+TablicaWynikow[0][kolejka]>10)){
+		if (numberOfPins>10 || (tableOfScore[0][round]+tableOfScore[0][round]>10)){
 			throw new IllegalStateException();
 		}
-		if (kolejka>=2 && TablicaWynikow[3][kolejka-2]==1){
-			DodajBonus(kolejka-2,numberOfPins);
-			TablicaWynikow[3][kolejka-2]=0;
+		if (round>=2 && tableOfScore[3][round-2]==1){
+			addBonus(round-2,numberOfPins);
+			tableOfScore[3][round-2]=0;
 		}
-		if (kolejka>=1 && TablicaWynikow[3][kolejka-1]==1){
-			DodajBonus(kolejka-1,numberOfPins);
-			TablicaWynikow[3][kolejka-1]=0;
+		if (round>=1 && tableOfScore[3][round-1]==1){
+			addBonus(round-1,numberOfPins);
+			tableOfScore[3][round-1]=0;
 		}
-		if (kolejka>=1 && TablicaWynikow[3][kolejka-1]==2){
-			DodajBonus(kolejka-1,numberOfPins);
-			TablicaWynikow[3][kolejka-1]=1;
+		if (round>=1 && tableOfScore[3][round-1]==2){
+			addBonus(round-1,numberOfPins);
+			tableOfScore[3][round-1]=1;
 		}
-		TablicaWynikow[rzut][kolejka]=numberOfPins;
-		TablicaWynikow[2][kolejka]=TablicaWynikow[2][kolejka]+numberOfPins;
-		if (rzut==0 && numberOfPins == 10){
-			TablicaWynikow[3][kolejka]=2;
-			kolejka++;
+		tableOfScore[roll][round]=numberOfPins;
+		tableOfScore[2][round]=tableOfScore[2][round]+numberOfPins;
+		if (roll==0 && numberOfPins == 10){
+			tableOfScore[3][round]=2;
+			round++;
 		}
-		else if (TablicaWynikow[2][kolejka]==10){
-			TablicaWynikow[3][kolejka]=1;
-			rzut=0;
-			kolejka++;
+		else if (tableOfScore[2][round]==10){
+			tableOfScore[3][round]=1;
+			roll=0;
+			round++;
 		}
-		else if (rzut==1){
-			rzut=0;
-			kolejka++;
+		else if (roll==1){
+			roll=0;
+			round++;
 		}else{
-			rzut++;
+			roll++;
 		}
 	}
 	public int score(){
 		int score=0;
-		for (int i=0;i<kolejka;i++){
-				score = score + TablicaWynikow[2][i];
+		for (int i=0;i<round;i++){
+				score = score + tableOfScore[2][i];
 		}
 		return score;
 	}
 	public boolean isFinished(){
-		if (kolejka<10){
+		if (round<10){
 			return false;
 		}
-		else if (kolejka==10 && TablicaWynikow[2][9]<10){
+		else if (round==10 && tableOfScore[2][9]<10){
 			return true;
-		}else if(kolejka==11 && TablicaWynikow[2][9]>=10 && TablicaWynikow[2][9]!=2){
+		}else if(round==11 && tableOfScore[2][9]>=10 && tableOfScore[2][9]!=2){
 			return true;
-		}else if(kolejka==12){
+		}else if(round==12){
 			return true;
 		}
 		return false;
 	}
-	public void DrukujTablice(){
-		for (int i=0;i<kolejka;i++){
+	public void printTable(){
+		for (int i=0;i<round;i++){
 			for(int j=0;j<=3;j++){
-				System.out.println(TablicaWynikow[j][i]);
+				System.out.println(tableOfScore[j][i]);
 			}
 			System.out.println(" ");
 		}
